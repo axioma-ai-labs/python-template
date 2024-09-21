531470adb2aec9edb48445674f9a4f9615d231da
Dzmitry Hramyka <grom.dima.grom@gmail.com>
2024-09-21 20:56:07 +0200

    Example project setup

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from src.core.config import settings
+
+
+def main() -> None:
+    """
+    Main entry point for the application. This is example docstring following Google style.
+
+    Returns:
+        None
+    """
+    print(settings.PROJECT_NAME)
+
+
+if __name__ == "__main__":
+    main()
